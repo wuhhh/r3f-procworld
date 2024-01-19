87c62cbe60d62bd991f5def150440a31f83f255d
Huw Roberts <huwrroberts@gmail.com>
2024-01-19 13:37:12 +0000

    Comment world frag

--- src/shaders/world/frag.glsl
+++ src/shaders/world/frag.glsl
@@ -17,19 +17,40 @@ void main() {
 	float speed = .45;
 	float xp = 32.; // x period
 	float yp = 1.; // y period
+
+	// Multiply the UVs by the period to get the repeating pattern
 	vec2 uv = vec2(vUv.x * xp, vUv.y * yp);
+
+	// Add time to y to make it travel
 	uv.y += uTime * speed;
-	vec3 base = mix(uCapsuleColourNear, uCapsuleColourFar, vUv.y); // main colour mix
-	vec3 brighter = brightnessContrast(base, .2, 1.); // brighter version of main colour
-	float noise = psrdnoise(vec3(uv.x, uv.y, uTime * .1), vec3(xp, yp, 0.)); // periodic noise
+
+	// Main colour mix
+	vec3 base = mix(uCapsuleColourNear, uCapsuleColourFar, vUv.y);
+
+	// Brighter version of main colour
+	vec3 brighter = brightnessContrast(base, .2, 1.); 
+
+	//  Periodic noise (wraps seamlessly around tubes, spheres etc.)
+	float noise = psrdnoise(vec3(uv.x, uv.y, uTime * .1), vec3(xp, yp, 0.)); 
+
+	// Alpha feathering
 	float stepWidth = .06;
 	float stepStart = .63;
-	float alpha = smoothstep(stepStart, stepStart + stepWidth, noise); // alpha feathering
-	alpha *= sin(vUv.x * 16.) * .5 + .5; // alpha variation around x
+	float alpha = smoothstep(stepStart, stepStart + stepWidth, noise); 
+
+	// Vary alpha around x
+	alpha *= sin(vUv.x * 16.) * .5 + .5; 
 	alpha *= 1. - pow(vUv.y, 4.0);
+
+	// Mix the base colour with the brighter colour
 	vec3 blend = blendLighten(base, brighter, 1.);
-	vec4 postColour = mix(vec4(base, 1. - pow(vUv.y, 16.0)), vec4(blend, alpha), vLevel.y); // capsule/cloud mix
+
+	// Mix between capsule and cloud
+	vec4 postColour = mix(vec4(base, 1. - pow(vUv.y, 16.0)), vec4(blend, alpha), vLevel.y);
+
+	// Add some brighter patches using elevation from the vertex shader
 	postColour = brightnessContrast(postColour, pow(vElevation, 2.0) * .25, 1.05);
+	
 	gl_FragColor = postColour;
 	// gl_FragColor = vec4(vUv.x, vUv.y, 0., 1.);
 } 
