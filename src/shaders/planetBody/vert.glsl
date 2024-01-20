65c8122ffc8f9ca3c497236c1e832b3bda82bcfa
Huw Roberts <huwrroberts@gmail.com>
2024-01-20 16:00:16 +0000

    Use highp precision

--- src/shaders/planetBody/vert.glsl
+++ src/shaders/planetBody/vert.glsl
@@ -1,4 +1,4 @@
-precision mediump float;
+precision highp float;
 
 varying vec3 vNormal;
 varying vec2 vUv;
--- src/shaders/spaceCanvas/frag.glsl
+++ src/shaders/spaceCanvas/frag.glsl
@@ -1,4 +1,4 @@
-precision mediump float;
+precision highp float;
 
 uniform vec3 uColor;
 uniform vec3 uColor1;
